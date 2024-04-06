c6e58a6b903055fe1fdc639cb1e18a212883b424
Adriano Gaiotto de Oliveira <adriano.gaiotto@gmail.com>
2024-04-05 21:17:31 -0300

    update-adicionado tarefa de listar series

--- src/main/java/br/com/agdeo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/agdeo/screenmatch/principal/Principal.java
@@ -23,35 +23,49 @@ public class Principal {
     private ConsumoAPI consumo = new ConsumoAPI();
     private ConverteDados conversor = new ConverteDados();
 
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
     public void exibirMenu() {
-        var menu = """
-                1 - Buscar séries
-                2 - Buscar episódios
-                                
-                0 - Sair                                 
-                """;
+        var opcao = -1;
+        while(opcao != 0) {
+            var menu = """
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries buscadas
+                                    
+                    0 - Sair                                 
+                    """;
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
+    }
+
+    private void listarSeriesBuscadas() {
+        dadosSeries.forEach(System.out::println);
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -74,4 +88,14 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
     }
+
+//    public static Categoria fromPortugues(String text) {
+//        for (Categoria categoria : Categoria.values()) {
+//            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
+//                return categoria;
+//            }
+//        }
+//        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+//    }
+
 }
